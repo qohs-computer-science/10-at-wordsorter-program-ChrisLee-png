e7eeba6bae0b3cb9a00d5b301b9741daf232db0c
Chris Lee <chriswole19@gmail.com>
2025-10-10 16:45:44 +0000

    test

--- WordSorter.java
+++ WordSorter.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Scanner;
 public class WordSorter 
 {
@@ -14,7 +15,13 @@ public class WordSorter
 			System.out.println("Cannot find file... Exiting Program");
 			return;
 		}	
+
 		String word = "";
+		ArrayList [] leo = new ArrayList[27];
+		for(int l=0;l<leo.length;l++){
+			leo[l]= new ArrayList<String>();
+		}
+
 		while(in.hasNext())
 		{
 			word = in.next();
@@ -28,9 +35,49 @@ public class WordSorter
 			word = word.replace("!", "");
 			word = word.replace("?", "");
 			word = word.replace("'", "");
-			System.out.println(word);
-			
-			
+			word = word.toLowerCase();
+			int meade=word.substring(0,1).compareTo("a");
+			leo[meade].add(word);
+			for(int i=0;i<leo[meade].size()-1;i++){
+				if(word.equals(leo[meade].get(i)))
+					leo[meade].remove(leo[meade].size()-1);
+			}
+		}
+		Scanner inp = new Scanner(System.in);
+		System.out.println("Enter in some of your own words, when you want to stop enter 'x'");
+		String kat="";
+		while(!(kat.equals("x"))){
+			kat =inp.nextLine();
+			if(!(kat.equals("x")))
+				leo[26].add(kat);
+		}
+		System.out.println("You now have the following options: \n1. Print out all words starting with a specific letter \n2. Print out all words \n3. Print out the total number of unique words \n4. Search and determine if a word is in the article \n5. Remove a word from the data structure \n6. Exit");
+		int nik = inp.nextInt();
+		boolean col = true;
+		while(col){
+			if(nik==1){
+				System.out.print("enter a lowercase letter: ");
+				inp.nextLine()
+				for(int k=0;k<leo[inp.nextLine().compareTo("a")].size(); k++){
+					System.out.print(leo[26].get(k)+" ");
+				}
+			}
+			else if(nik==2){
+
+			}
+			else if(nik==3){
+
+			}
+			else if(nik==4){
+
+			}
+			else if(nik==5){
+
+			}
+			else{
+				col = false;
+			}
+
 		}
 	}
 }
